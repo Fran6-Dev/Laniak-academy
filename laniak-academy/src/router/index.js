54ff818c7f65133300bf5e7b60a1876de494b588
Francis <francis@efashion-paris.com>
2025-05-08 01:28:50 +0200

    gestion page contact

--- laniak-academy/src/router/index.js
+++ laniak-academy/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import About from '../pages/AboutUs.vue'
 import Program from '../pages/Program.vue'
@@ -46,7 +46,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
--- laniak-academy/vite.config.js
+++ laniak-academy/vite.config.js
@@ -4,4 +4,23 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['vue', 'vue-router', 'vue-i18n']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
+  server: {
+    port: 3000
+  },
+  base: '/',
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
