418f3504ba4f2590eaa8c0b702a5c796361fc30d
Francis <francis@efashion-paris.com>
2025-05-08 02:17:07 +0200

    update

--- laniak-academy/src/components/Navbar.vue
+++ laniak-academy/src/components/Navbar.vue
@@ -66,7 +66,7 @@
               class="border-transparent text-gray-500 hover:border-orange-500 hover:text-orange-500 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
               active-class="border-orange-500 text-orange-500"
             >
-              Contact Us
+              {{ $t('nav.contactUs') }}
             </RouterLink>
           </div>
         </div>
@@ -188,7 +188,7 @@
             class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
             active-class="bg-orange-50 border-orange-500 text-orange-700"
           >
-            Contact Us
+            {{ $t('nav.contactUs') }}
           </RouterLink>
           <!-- Language Selector in Mobile Menu -->
           <div class="px-6 py-2">
