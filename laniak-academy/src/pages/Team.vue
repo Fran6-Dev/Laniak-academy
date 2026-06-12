4c4d281d0adb6e10af38a6003006eef5d6148b64
Francis Lukoki <francislukoki@hotmail.fr>
2026-06-12 02:06:41 +0200

    correction img

--- laniak-academy/src/pages/Team.vue
+++ laniak-academy/src/pages/Team.vue
@@ -46,7 +46,7 @@
             <div class="px-4 py-5 sm:p-6">
               <div class="flex flex-col items-center">
                 <div class="h-32 w-32 rounded-full bg-orange-100 flex items-center justify-center mb-4">
-                  <img src="../assets/abdel.jpeg" :alt="$t('team.members.abdel.name')" class="w-full h-full object-cover rounded-full">
+                  <img src="../assets/Abdel.jpeg" :alt="$t('team.members.abdel.name')" class="w-full h-full object-cover rounded-full">
                 </div>
                 <h3 class="text-lg font-medium text-black">{{ $t('team.members.abdel.name') }}</h3>
                 <p class="text-orange">{{ $t('team.members.abdel.role') }}</p>
