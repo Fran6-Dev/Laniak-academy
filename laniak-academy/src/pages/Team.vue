12b7e0d6b3373ce2ce96523d9dc116c76319bad0
Francis Lukoki <francislukoki@hotmail.fr>
2026-06-12 02:09:21 +0200

    correction img amandine

--- laniak-academy/src/pages/Team.vue
+++ laniak-academy/src/pages/Team.vue
@@ -62,7 +62,7 @@
             <div class="px-4 py-5 sm:p-6">
               <div class="flex flex-col items-center">
                 <div class="h-32 w-32 rounded-full bg-orange-100 flex items-center justify-center mb-4">
-                  <img src="../assets/amandine.jpeg" :alt="$t('team.members.amandine.name')" class="w-full h-full object-cover rounded-full">
+                  <img src="../assets/Amandine.jpeg" :alt="$t('team.members.amandine.name')" class="w-full h-full object-cover rounded-full">
                 </div>
                 <h3 class="text-lg font-medium text-black">Amandine Renault</h3>
                 <p class="text-orange">{{ $t('team.members.amandine.role') }}</p>
